fe8dddd20b7da544389c5a37ed8fdb7a2fb3ca82
citra-rahman <citrapuspita.r@gmail.com>
2024-07-02 20:57:43 +0300

    ss

--- src/App.js
+++ src/App.js
@@ -4,29 +4,36 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 import { createTodo } from "./graphql/mutations";
-import { generateClient } from 'aws-amplify/api';
+import { generateClient } from "aws-amplify/api";
 
 Amplify.configure(awsExports);
-async function storeToDo(){
-  const client = generateClient();
-  const createdTodo = await client.graphql({
-    query: createTodo,
-    input: {
-      name: 'Fix UI Bugs',
-      description: 'Fix UI Bug Both on frontend and backend'
-    }
-  });
-  
-}
-const App = () => {
 
+async function storeToDo() {
+  try {
+    const client = generateClient();
+    await client.graphql({
+      query: createTodo,
+      variables: {
+        input: {
+          id: '2',
+          name: "Fix UI Bugs",
+          description: "Fix UI Bug Both on frontend and backend",
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const App = () => {
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <main>
           <h1>Welcome {user.username}</h1>
           <button onClick={signOut}>Sign Out</button>
-          <button onClick={storeToDo}></button>
+          <button onClick={storeToDo}>New To Do</button>
         </main>
       )}
     </Authenticator>
