d18313549c7cc13427b692c6635dee8b362237dd
citra-rahman <citrapuspita.r@gmail.com>
2024-07-02 21:17:51 +0300

    add fetch to do

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
 import { generateClient } from "aws-amplify/api";
 
 Amplify.configure(awsExports);
 
+const client = generateClient();
+
 async function storeToDo() {
   try {
-    const client = generateClient();
     await client.graphql({
       query: createTodo,
       variables: {
@@ -26,6 +28,17 @@ async function storeToDo() {
   }
 }
 
+async function fetchToDo(){
+  try{
+    const result = await client.graphql({
+      query: listTodos
+    });
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 const App = () => {
   return (
     <Authenticator>
@@ -34,6 +47,7 @@ const App = () => {
           <h1>Welcome {user.username}</h1>
           <button onClick={signOut}>Sign Out</button>
           <button onClick={storeToDo}>New To Do</button>
+          <button onClick={fetchToDo}>Fetch To Do</button>
         </main>
       )}
     </Authenticator>
